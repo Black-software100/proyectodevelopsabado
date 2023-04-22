a8e8e233e09a194532d1c0295d927a99ac9c8029
juan esteban torres ibarguen <jt158161@gmail.com>
2023-04-22 13:05:14 -0500

    Se instacia el objecto

--- Toronja.py
+++ Toronja.py
@@ -1,11 +1,46 @@
-class Toranja:
+class Toronja:
 
     def __init__(self):
-        self.nombre = None
-        self.cantidad=None
-        self.precio = None
-        self.vitaminaAportada=None
+        self.__nombre = None
+        self.__cantidad=None
+        self.__precio = None
+        self.__vitaminaAportada=None
+
+    #DECLARANDO EL METODO GET(OBTENGO/MUESTRO UN ATRIBUTO)
+    @property
+    def nombre(self):
+        return self.__nombre
     
+    #DECLARANDO EL METODO GET(CONFIGURO,LLEVO UN VALOR A UN ATRIBUTO)
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre = dato
+
     def agregarFrutar(self):
 
         print("Se pela y se hecha")
+
+    
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @cantidad.setter
+    def cantidad(self,dato):
+        self.__cantidad = dato
+
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @precio.setter
+    def precio(self,dato):
+        self.__precio = dato
+
+    @property
+    def vitaminaAportada(self):
+        return self.vitaminaAportada
+    
+    @vitaminaAportada.setter
+    def vitaminaAportada(self,dato):
+        self.__vitaminaAportada = dato
---
+++ main.py
@@ -0,0 +1,16 @@
+from Toronja import Toronja
+
+objecto = Toronja()
+
+objecto.nombre = "Toronja"
+
+print(objecto.nombre)
+
+objecto.cantida = 10
+print(objecto.cantida)
+
+objecto.precio = 5000
+print(objecto.precio)
+
+objecto.vitaminaAportada = "C"
+print(objecto.vitaminaAportada)
